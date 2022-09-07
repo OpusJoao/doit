d02fd17519e51411f12efbbeb9cf2f21b600d5b7
João Ferreira <joao.ferreira@gruponewway.com.br>
2022-09-06 22:16:40 -0300

    refactor: ensure todo component change the todoContext when clicked on checkbox

--- pages/components/todoBox/index.js
+++ pages/components/todoBox/index.js
@@ -1,35 +1,22 @@
 import styles from '../../../styles/ToDo.module.css'
-import { toast } from 'react-toastify'
 import { useState } from 'react'
-function ToDoBox(prop) {
+function ToDoBox(props) {
   const backgroundColorsByStatus = {
     complete: '#35a261',
     inProgress: '#a7a7a7',
     uncomplete: '#e05f5f'
   }
-  const [isCompleted, setIsCompleted] = useState(prop.status == 'complete')
   function returnBackgroundByStatus(status){
     return backgroundColorsByStatus[status] || backgroundColorsByStatus['inProgress'] 
   }
 
-  function handleClickCheck(e){
-
-    if(prop.status !== 'complete'){
-      setIsCompleted(!isCompleted)
-      toast.success('Tarefa completa com sucesso!', {position: 'bottom-right'})
-    }else{
-      toast.error('Não é possível completar tarefa. Tarefa já completa', {position: 'bottom-right'})
-    }
-    
-  }
-
   return (
-      <div className={styles.todo_box} style={{ backgroundColor: returnBackgroundByStatus(prop.status) }}>
+      <div className={styles.todo_box} style={{ backgroundColor: returnBackgroundByStatus(props.status) }}>
         <div className={styles.todo_box_action}>
-          <input type='checkbox' id={`todoboxInput${prop.id}`} key={prop.id} onClick={handleClickCheck} checked={isCompleted} readOnly={isCompleted}/>
+          <input type='checkbox' id={`todoboxInput${props.id}`} key={props.id} onClick={()=>props.onClick(props.id)} checked={props.status == 'complete'} readOnly={props.status == 'complete'}/>
         </div>
         <div className={styles.todo_box_body}>
-          <span>{prop.text}</span>
+          <span>{props.text}</span>
         </div>
         
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,43 @@ import { useTodo } from '../lib/TodoContext'
 import styles from '../styles/Home.module.css'
 import Box from './components/box'
 import ToDoBox from './components/todoBox'
+import { toast } from 'react-toastify'
+
 export default function Home() {
   const [currentTodo, setCurrentTodo] = useState('')
   const {todos, setTodos} = useTodo()
 
   function addTodo(){
     let currentTodos = todos
-    let newTodos = [... currentTodos, {text: currentTodo, status: 'inProgress'}]
+    let lastId = currentTodos[currentTodos.length - 1]?.id 
+    lastId = !!lastId ? lastId : 0
+    let newTodos = [... currentTodos, {id: lastId + 1, text: currentTodo, status: 'inProgress'}]
     setTodos(newTodos)
     setCurrentTodo('')
+  }
+
+  function handleClickCheck(idTodo){
+    //getByIdTodo
+    const currentTodo = todos.filter(todo => todo?.id === idTodo).pop()
+    if(currentTodo?.status !== 'complete'){
+    //update no context api
+      const todosUpdated = todos.map(todo => {        
+        if(todo?.id == idTodo){
+          return {...todo, status: 'complete'}
+        }
+        return todo
+      })
+      setTodos(todosUpdated)
+      toast.success('Tarefa completa com sucesso!', {position: 'bottom-right'})
+    }else{
+      toast.error('Não é possível completar tarefa. Tarefa já completa', {position: 'bottom-right'})
+    }
+  }
+
+  function handleOnKeyDown(e){
+    if(e.key === 'Enter'){
+      addTodo()
+    }
   }
 
   return (
@@ -20,7 +48,7 @@ export default function Home() {
         <div className={styles.row}>
           <h1>Doit</h1>
           <Box style={{ marginTop: '2rem', display: 'flex' }}>
-            <input className={styles.input} placeholder="O que deseja fazer?" value={currentTodo} onChange={(e) => setCurrentTodo(e.target.value)}/>
+            <input className={styles.input} placeholder="O que deseja fazer?" value={currentTodo} onChange={(e) => setCurrentTodo(e.target.value)} onKeyDown={handleOnKeyDown}/>
             <button className={styles.button} style={{ margintop: '1rem' }} onClick={addTodo}>Adicionar</button>
           </Box>
           <Box style={{ marginTop: '1.5rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -28,6 +56,8 @@ export default function Home() {
             {todos.map(val => <ToDoBox 
               text={val?.text}
               status={val?.status}
+              id={val?.id}
+              onClick={handleClickCheck}
               />)}
           </Box>
         </div>
